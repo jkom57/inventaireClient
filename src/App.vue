f2df6d109c790cadfb6b4303e8a9d2ee938198b5
Joshua Komaba Chávez <2210113@etu.cegepjonquiere.ca>
2022-03-09 09:49:06 -0500

    Starting the navbar

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,41 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "@/components/HelloWorld.vue";
+import NavBar from "@/components/navbar.vue"
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
+  <div>
+    <NavBar />
+    <!--<div>
+      <header>
+        <img
+          alt="Vue logo"
+          class="logo"
+          src="@/assets/logo.svg"
+          width="125"
+          height="125"
+        />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+        <div class="wrapper">
+          <HelloWorld msg="You did it!" />
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+          <nav>
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/about">About</RouterLink>
+          </nav>
+        </div>
+      </header>
 
-  <RouterView />
+      <RouterView />
+    </div>-->
+  </div>
 </template>
 
 <style>
 @import "@/assets/base.css";
 
-#app {
+/*#app {
   max-width: 1280px;
   margin: 0 auto;
   padding: 2rem;
@@ -121,5 +127,5 @@ nav a:first-of-type {
     padding: 1rem 0;
     margin-top: 1rem;
   }
-}
+}*/
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="navbar">
+        <div class="title">
+            <h1 class="titleText">
+                Inventaire
+            </h1>
+        </div>
+        <div class="recherche">
+            <input type="text" placeholder="Search..">
+        </div>
+        <div class="button">
+            <CirclePlusIcon />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import Vue from 'vue'
+import CirclePlusIcon from "../components/icons/iconPlusCircle.vue"
+/*export default Vue.extend({
+    
+})*/
+</script>
+
+<style scoped>
+.navbar {
+    background-color: rgb(73, 43, 19);
+    width: 100%;
+}
+.titleText {
+    color: white;
+    font-weight: bold;
+    font-size: xx-large
+}
+</style>
